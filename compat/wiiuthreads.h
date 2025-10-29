9a7f25e140e30b86fe5e302ee3fbd8114a987cbd
jranderia3 <94210600+jranderia3@users.noreply.github.com>
2025-10-29 01:15:21 -0400

    most of thread implemented, currently using reference implementation for pthread_create; TODO: verify it actually works

--- compat/wiiuthreads.h
+++ compat/wiiuthreads.h
@@ -24,7 +24,8 @@ typedef OSThreadAttributes pthread_attr_t;
 // "Similar to OSMutex but tries to acquire the mutex 
 // without using the global scheduler lock, 
 // and does not test for thread cancel"
-// Use this? idk
+// Use that instead? Idk so I'll use this for now,
+// as it seems safer
 typedef OSMutex pthread_mutex_t;
 
 // docs say something about OScondition
@@ -41,5 +42,131 @@ typedef struct {
     OSThread mtx;
 } pthread_once_t;
 
+#define ONCE_VALUE_INIT 0
+#define ONCE_VALUE_STARTED 1
+#define ONCE_VALUE_DONE 2
+
+static int pthread_create(pthread_t *thread, const pthread_mutex_t *attr,
+                          void *(*start_routine)(void*), void *arg)
+{
+   OSThread *thread = (OSThread *)memalign(16, sizeof(OSThread));
+   if (!thread) {
+      return ENOMEM;
+   }
+
+   memset(thread, 0, sizeof(OSThread));
+
+   char *stack = (char *)memalign(16, __wut_thread_default_stack_size);
+   if (!stack) {
+      free(thread);
+      return ENOMEM;
+   }
+
+   if (!OSCreateThread(thread,
+                       (OSThreadEntryPointFn)entryPoint,
+                       (int)entryArgs,
+                       NULL,
+                       stack + __wut_thread_default_stack_size,
+                       __wut_thread_default_stack_size,
+                       16,
+                       OS_THREAD_ATTRIB_AFFINITY_ANY)) {
+      free(thread);
+      free(stack);
+      return EINVAL;
+   }
+
+   *outThread = thread;
+   OSSetThreadDeallocator(thread, &__wut_thread_deallocator);
+   OSSetThreadCleanupCallback(thread, &__wut_thread_cleanup);
+
+   // Set a thread run quantum to 1 millisecond, to force the threads to
+   // behave more like pre-emptive scheduling rather than co-operative.
+   OSSetThreadRunQuantum(thread, 1000);
+
+   OSResumeThread(thread);
+   return 0;
+}
+
+static int pthread_join(pthread_t thread, void **value_ptr)
+{
+    if (!OSJoinThread(*thread, (int *)*retval)) {
+        return EINVAL;
+    }
+    return 0;
+}
+
+static int pthread_mutex_init(pthread_mutex_t *mutex,
+                              const pthread_mutexattr_t *attr)
+{
+    return OSInitMutex(mutex);
+}
+
+static int pthread_mutex_destroy(pthread_mutex_t *mutex)
+{
+    // no function to destroy mutex on cafe os
+    // https://wut.devkitpro.org/mutex_8h.html
+    // so just return 0 lmao
+    return 0;
+}
+
+static int pthread_mutex_lock(pthread_mutex_t *mutex)
+{
+    OSLockMutex(mutex);
+    return 0;
+}
+
+static int pthread_mutex_unlock(pthread_mutex_t *mutex)
+{
+    OSUnlockMutex(mutex);
+    return 0;
+}
+
+static int pthread_cond_init(pthread_cond_t *cond,
+                             const pthread_condattr_t *attr)
+{
+    OSInitCond(cond);
+}
+
+static int pthread_cond_destroy(pthread_cond_t *cond)
+{
+    // no function to destroy cond on cafe os
+    // https://wut.devkitpro.org/group__coreinit__cond.html
+    // so just return 0 lmao
+    return 0;
+}
+
+static int pthread_cond_signal(pthread_cond_t *cond)
+{
+    OSSignalCond(cond);
+    return 0;
+}
+
+static int pthread_cond_broadcast(pthread_cond_t *cond)
+{
+    // OSSignalCond is already a broadcast:
+    // OSSignalCond: 
+    // Will wake up any threads waiting on the condition with OSWaitCond. 
+    OSSignalCond(cond);
+    return 0;
+}
+
+static int pthread_cond_timedwait(pthread_cond_t *cond, 
+                                  pthread_mutex_t *mutex,
+                                  const struct timespec *abstime)
+{
+
+}
+
+static int pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex)
+{
+    OSWaitCond(cond, mutex);
+    return 0;
+}
+
+static int pthread_once(pthread_once_t *once_control,
+                        void (*init_routine)(void))
+{
+    
+}
 
 #endif
